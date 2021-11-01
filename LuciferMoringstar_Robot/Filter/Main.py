a6faab73fee883a99f088945a7d7aa0ab2f00c0d
9745561967 <82279792+9745561967@users.noreply.github.com>
2021-11-02 00:17:20 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**Requested Movie:** {search}\n**Requested by:** {mention}\n**📤 Uploaded by {message.chat.title}**"
+        mo_tech_yt = f"**🗂️ Requested Film:** {search}\n**🥇 Main Channel:** {random.choice(RATING)}\n**📤 Uploaded by:** {random.choice(GENRES)}**"
         files = await get_filter_results(query=search)
         if files:
             for file in files:
@@ -115,7 +115,7 @@ async def group(client, message):
     if 2 < len(message.text) < 50:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**Requested Movie:** {search}\n**Requested by:** {mention}\n**📤 Uploaded by {message.chat.title}**"
+        mo_tech_yt = f"**🗂️ Requested Film:** {search}\n**🥇 Main Channel:** {random.choice(RATING)}\n**📤 Uploaded by:** {random.choice(GENRES)}**"
         nyva=BOT.get("username")
         if not nyva:
             botusername=await client.get_me()
