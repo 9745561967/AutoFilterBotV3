3430e66689389d51f493c2754cadf8c171be9694
9745561967 <82279792+9745561967@users.noreply.github.com>
2021-11-01 23:55:41 +0530

    Update Index.py

--- LuciferMoringstar_Robot/Channel/Index.py
+++ LuciferMoringstar_Robot/Channel/Index.py
@@ -79,3 +79,6 @@ async def index_files(bot, message):
             else:
                 await msg.edit(f'Total {total_files} Saved To DataBase!')
 
+RATING = {mention} 
+GENRES = """<b>AM Movies Group</b>"""
+
