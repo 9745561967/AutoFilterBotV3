2d01f17849ccdbffa46e6918faee44d21118bb16
9745561967 <82279792+9745561967@users.noreply.github.com>
2022-07-14 12:29:52 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -10,8 +10,22 @@ from .Channel import (
    GENRES
 )
 
-HELP = """ <b>Sorry I can't help You</b> """
+HELP = """ 
+Basic Commads
 
+/start :- check am Alive
+
+Bot Owner Only
+/broadcast Replay Any Message or Media
+/stats user Status
+/ban_user  click ban_user more info
+/unban_user click unban_user more info
+/banned_users Banned User Details
+/total How Many Files Added In Database
+/logger  Get Logs
+/delete Delete File From Database
+
+"""
 
 
 ABOUT = """ <b>Nothing Great About Me!</b> """
