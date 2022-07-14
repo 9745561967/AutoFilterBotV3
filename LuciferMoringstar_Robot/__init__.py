7a085c0e74fb65f47b5bea2f49cacb069c2efd1a
9745561967 <82279792+9745561967@users.noreply.github.com>
2022-07-14 12:34:57 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -27,5 +27,17 @@ Bot Owner Only
 
 """
 
+ABOUT = """
+➥ Name :  Auto Filter V3
+    
+➥ Creator : [Naughty PROFESSOR](
+https://youtube.com/c/NaughtyPROFESSOR)
 
-ABOUT = """ <b>Nothing Great About Me!</b> """
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+"""
+
+
